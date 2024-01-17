6907b57190c0a4ff71392fda3d6a689e2c3a3299
tamaradikonic <147807959+tamaradikonic@users.noreply.github.com>
2024-01-17 12:54:19 +0100

    pola zadace
    .

---
+++ zadace/indeks.js
@@ -0,0 +1,8 @@
+var unesiBroj = prompt ("Unesi broj:")
+
+    if (unesiBroj > 10 ) {
+        alert("Broj koji je veći od 10");
+    }
+    if (unesiBroj < 10 ) {
+        alert("broj nije veći od 10");
+    }
